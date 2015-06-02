30bd71782326861b98d34afe67893419ac24b35a
emilykilen <m173198@usna.edu>
2015-06-02 10:57:55 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -5,13 +5,13 @@ Created on Fri May 29 16:38:46 2015
 @author: ekilen
 """
 
-from matplotlib.pyplot import plt
+import matplotlib.pyplot as plt
 
 #Parameters to change
 
 H_g = 6.00612e-7
 
-H2_ref = 9.9895232e-01
+H2_ref = 9.9895232e-03
 
 H20_g = 9.7934795e-04
 
@@ -63,6 +63,6 @@ labels = 'H_g', 'H2_ref', 'H20_g', 'N_g', 'NH_g', 'NO_g', 'N2_ref', 'O_g', 'OH-g
 fracs = [H, H2, H20, N, NH, NO, N2, O, OH, O2]
 explode = (0,.05,0,0)
 
-plt.pie(fracs, explode = explode, labels = labels,autopct = '%1.1f%%',shadow = true, startangle =90)
+plt.pie(fracs, explode = None, labels = labels,autopct = None,pctdistance=1.5,shadow = False,labeldistance=1.5, startangle =None,radius=None)
 
 plt.show()
